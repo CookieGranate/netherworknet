745971c4681b155b8334768b34a1dc48d8cb4273
CookieGranate <cookiesooslol@gmail.com>
2023-03-25 17:18:29 +0100

    navbar angefangen aber tailwind will nicht

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 // pages
 import Home from './pages/Home'
+// components
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <main className='w-full h-full'>
+      <Navbar />
       <Routes>
 
         <Route path='/' element={<Home />} />
 
+        {/* <Route path='*' element={<NotFound />} /> */}
+
       </Routes>
+    </main>
+      
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import logo from '../img/NW_logo.jpg'
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <header className="h-20 w-full top-0 absolute">
+            <nav>
+                <div id="left">
+                    <Link to={'/'}>
+                        <img className='max-h-14 align-middle' src={logo} alt="logo" />
+                        Netherwork.net
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
