dd3c9935e4d88319b79686071fa3716227e0283a
TheRealMangoAPI <ricardopernack@gmail.com>
2023-03-25 14:56:22 +0100

    lol fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-const { BrowserRouter, Routes, Route } = require('react-router-dom')
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 // pages
 import Home from './pages/Home'
