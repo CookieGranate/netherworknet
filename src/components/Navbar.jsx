ebbbe572840f914e0cab523047f0e75c2d3aebe5
CookieGranate <cookiesooslol@gmail.com>
2023-03-25 20:18:13 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import logo from '../img/NW_logo.jpg'
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <header className="w-full h-20 absolute top-0">
+            <nav>
+                <div id="left" className="absolute left-0">
+                    <Link to={'/'}>
+                        <img src={logo} alt="logo" className="w-16 align-middle" />
+                        Netherworks.net
+                    </Link>
+                </div>
+                <div className="absolute left-1/2" id="middle">
+
+                </div>
+            </nav>
+        </header>
     )
 }
 
