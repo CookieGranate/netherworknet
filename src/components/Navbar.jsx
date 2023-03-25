c16c33ce776aa9592d8ae4261a1f4029ac279bf1
CookieGranate <cookiesooslol@gmail.com>
2023-03-25 17:20:51 +0100

    reverse push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                         Netherwork.net
                     </Link>
                 </div>
-            </nav>test
+            </nav>
         </header>
     )
 }
